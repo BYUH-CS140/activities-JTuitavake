f8befb469741fdab2b308b29c66c7b502a71b7a9
JTuitavake <jayp2021@go.byuh.edu>
2025-06-14 21:32:59 -1000

    changes

--- Activity-15/script.js
+++ Activity-15/script.js
@@ -1,5 +1,3 @@
-<script>
-    document.getElementById('changeColorButton').addEventListener('click', function() {
+document.getElementById('changeColorButton').addEventListener('click', function() {
     document.body.style.backgroundColor = '#' + Math.floor(Math.random()*16777215).toString(16);
 });
-</script>
